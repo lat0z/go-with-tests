deb6b417c1a990c36ffe68c9e7f72f65ef7dc62b
latoz <latoz@protonmail.com>
2021-07-11 19:20:44 -0500

    adding more sub tests, and extra arguments to the main program

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,10 +3,38 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("me")
-	want := "Hello, me"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage :=func(t testing.TB, got, want string){
+		//t.Helper is needed to tell the test suite this is a helper.
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q ", got, want)
+		}
 	}
+
+	t.Run("Saying hello to people", func(t *testing.T){
+		assertCorrectMessage(t, Hello("me",""), "Hello, me")
+	})
+
+	t.Run("Say 'Hello, World' when an empty string is supplied", func(t *testing.T){
+		got :=Hello("","")
+		want := "Hello, world"
+		if got !=want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("In spanish ", func(t *testing.T){
+		got :=Hello("me","Spanish")
+		want:= "Hola, me"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("In french ", func(t *testing.T){
+		got :=Hello("me","French")
+		want:= "Bonjour, me"
+		assertCorrectMessage(t, got, want)
+	})
+
+
 }
--- hello-world/main.go
+++ hello-world/main.go
@@ -2,12 +2,25 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix  = "Bonjour, "
 
-func Hello(name string) string {
+func Hello(name, language string) string {
+	if name == ""{
+		name = "world"
+	}
+	if language == spanish {
+		return spanishHelloPrefix + name
+	}
+	if language == french {
+		return frenchHelloPrefix + name
+	}
 	return englishHelloPrefix + name
 }
 
 func main(){
-	fmt.Println(Hello("me"))
+	fmt.Println(Hello("world",""))
 	}
